837fa6113f48107482102fd96d7b3ada46b449c9
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-11 19:15:42 +0100

    Creazione dei file css per hero.jsx e navbar.jsx - inserimento della logica per l'apertura e chiusura dell'hamburger menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import data from '../data/data.json/';
+import { useState } from 'react';
 
 const Navbar = () => {
     // ESTRAIAMO IL LOGO E L'ARRAY DEI LINK
     const { logo, navLinks } = data.navbar;
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="navbar">
+        <>
+            <nav className="navbar">
 
-            <div className="nav-logo">
-                <h1>{logo}</h1>
-            </div>
+                <div className="nav-logo">
+                    <h1>{logo}</h1>
+                </div>
 
-            <ul className="nav-links">
-                {/* USIAMO .MAP() PER CREARE I LINK DINAMICAMENTE*/}
-                {navLinks.map((link, index) => (
-                    <li key={index}>
-                        <a href="{link.path}">{link.label}</a>
-                    </li>
-                ))}
-            </ul>
+                {/* HAMBURGER ICON */}
+                <div classname={`nav-hamburger ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
 
-        </nav>
+                {/* Menu Links */}
+                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                    {/* USIAMO .MAP() PER CREARE I LINK DINAMICAMENTE*/}
+                    {navLinks.map((link, index) => (
+                        <li key={index} onClick={() => setIsOpen(false)}>
+                            <a href="{link.path}">{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+            </nav>
+        </>
     )
 }
 
