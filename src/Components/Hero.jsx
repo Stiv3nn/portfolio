260c8efb1dbba95fd580fcdc4b2b363d1187b6b2
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-11 21:26:24 +0100

    Aggiunto nuovo contenitore per il testo e per l'immagine

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import data from '../data/data.json';
+import '../Components/Hero.css'
 
 const Hero = () => {
     // Estraiamo i dati che ci servono dal "database" ---> data.json
     const { title, subtitle, description, ctaPrimary } = data.hero;
     return (
         <section className="hero">
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-            <p>{description}</p>
-            <button>{ctaPrimary}</button>
+            <div className="hero-container">
+                <div className="hero-content">
+                    <h1 className='hero-title'>{title}</h1>
+                    <h2 className='hero-subtitle'>{subtitle}</h2>
+                    <p className='hero-description'>{description}</p>
+                    <button className='hero-btn'>{ctaPrimary}</button>
+                </div>
+                <div className="hero-visual">
+                    <div className="glow-circle"></div>
+                </div>
+            </div>
         </section>
     );
 };
