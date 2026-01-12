f7f1c80ddbbb1eeb0717d266001409d0f886642f
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-12 21:53:07 +0100

    inserito sezione per l'immagine

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import '../Components/Hero.css'
 
 const Hero = () => {
     // Estraiamo i dati che ci servono dal "database" ---> data.json
-    const { title, subtitle, description, ctaPrimary } = data.hero;
+    const { title, subtitle, description, ctaPrimary, profileImage } = data.hero;
     return (
         <section className="hero">
             <div className="hero-container">
@@ -14,7 +14,11 @@ const Hero = () => {
                     <p className='hero-description'>{description}</p>
                     <button className='hero-btn'>{ctaPrimary}</button>
                 </div>
+
+                {/* INSERIRE L'IMMAGINE */}
                 <div className="hero-visual">
+                    <img src={profileImage} alt="Illustrazione di Stiven Hoxha" className='profile-illustration' />
+                    {/* IL GLOW-CIRCLE RIMARRà DIETRO L'IMMAGINE PER L'EFFETTO */}
                     <div className="glow-circle"></div>
                 </div>
             </div>
