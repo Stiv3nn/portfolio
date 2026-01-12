6bcc36cde1e14eda3bcf7703919069ba7740632c
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-12 22:07:31 +0100

    inserimento degli elementi per il pallino e l'effetto glow animato

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
-import data from '../data/data.json/';
+import data from '../data/data.json';
 import { useState } from 'react';
-import '../Components/Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
-    // ESTRAIAMO IL LOGO E L'ARRAY DEI LINK
     const { logo, navLinks } = data.navbar;
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <>
-            <nav className="navbar">
-
-                <div className="nav-logo">
-                    {logo}
+        <nav className="navbar">
+            {/* LOGO + STATUS DOT */}
+            <div className="nav-logo">
+                {logo}
+                <div className="status-container">
+                    <span className="status-dot"></span>
+                    <span className="status-text">Tech-ready!</span>
                 </div>
+            </div>
 
-                {/* HAMBURGER ICON */}
-                <div className={`nav-hamburger ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+            {/* HAMBURGER ICON */}
+            <div
+                className={`nav-hamburger ${isOpen ? 'open' : ''}`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
 
-                {/* Menu Links */}
-                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
-                    {/* USIAMO .MAP() PER CREARE I LINK DINAMICAMENTE*/}
-                    {navLinks.map((link, index) => (
-                        <li key={index} onClick={() => setIsOpen(false)}>
-                            <a href={link.path}>{link.label}</a>
-                        </li>
-                    ))}
-                </ul>
-
-            </nav>
-        </>
-    )
-}
+            {/* MENU LINKS */}
+            <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                {navLinks.map((link, index) => (
+                    <li key={index} onClick={() => setIsOpen(false)}>
+                        <a href={link.path}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
 
 export default Navbar;
