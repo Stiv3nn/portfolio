7388cdd48034c62103b2abfaa161c92d13cfec9e
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-18 15:18:03 +0100

    importata nell'app.jsx e fuori dalle rotte

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
 import Projects from "./Components/Projects.jsx";
+import Footer from './Components/Footer.jsx'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/progetti" element={<Projects />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   )
