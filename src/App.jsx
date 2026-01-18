f1f81bdeb6abb7a9eebf63dba8dc0af0932f0efa
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-18 15:40:54 +0100

    il footer non apparirà nella homepage ma nelle altre pagine si

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
 import Projects from "./Components/Projects.jsx";
-import Footer from './Components/Footer.jsx'
+import Footer from './Components/Footer.jsx';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/progetti" element={<Projects />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  )
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/progetti" element={<Projects />} />
+      </Routes>
+
+      {/* Il footer scompare solo in Home */}
+      {location.pathname !== '/' && <Footer />}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+/* IMPORTA IL BROWSER ROUTER */
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* AVVOLGI L'APP NEL ROUTER */}
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
 )
