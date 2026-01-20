dd2f785b89ace029d39455d8f108d29df2b7937a
Stiven hoxha <stivenhoxha007@gmail.com>
2026-01-20 21:08:50 +0100

    implementazione di ScrollToTop su app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import Hero from "./Components/Hero.jsx";
 import Projects from "./Components/Projects.jsx";
 import Contact from './Components/Contact.jsx';
 import Footer from './Components/Footer.jsx';
+import About from './Components/About.jsx';
+import ScrollToTop from './Components/ScrollToTop.jsx';
 
 function App() {
   const location = useLocation();
 
   return (
     <div className="App">
+      {/* Reset dello scroll */}
+      <ScrollToTop />
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/progetti" element={<Projects />} />
         <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
 
       {/* Il footer scompare solo in Home */}
